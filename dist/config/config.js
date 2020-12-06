b951ed99c0a853675c24370b071f71f5beaee062
Alexis Lagniel <a_lagniel@etu-webschoolfactory.fr>
2020-12-06 17:39:16 +0100

    removing dist

--- dist/config/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = void 0;
-exports.config = {
-    "token": "Nzg1MTU2NTExODY4OTExNjE2.X8zv-w.RnK6YU7gGm_LoqU7ACn1aG6lVIg",
-    "prefix": "!yeslife"
-};
-//# sourceMappingURL=config.js.map
