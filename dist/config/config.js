d6ca0e2fe966a3f998c4bcc108be5d5558211459
Alexis Lagniel <a_lagniel@etu-webschoolfactory.fr>
2020-12-06 17:19:14 +0100

    intialized

---
+++ dist/config/config.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = void 0;
+exports.config = {
+    "token": "Nzg1MTU2NTExODY4OTExNjE2.X8zv-w.RnK6YU7gGm_LoqU7ACn1aG6lVIg",
+    "prefix": "!yeslife"
+};
+//# sourceMappingURL=config.js.map
